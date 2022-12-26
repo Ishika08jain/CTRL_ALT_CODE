f0555d172f3d7055e58e7b7da3a76c3bb42aad40
muskanlalwani <muskan.lalwani2020@vitbhopal.ac.in>
2022-12-26 16:49:54 +0530

    vibe

--- app/src/main/java/com/example/vibe/MainActivity.java
+++ app/src/main/java/com/example/vibe/MainActivity.java
@@ -1,14 +1,61 @@
 package com.example.vibe;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    HomeFragment homeFragment=new HomeFragment();
+    PlaylistFragment playlistFragment = new PlaylistFragment();
+    settingFragment settingFragment= new settingFragment();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        bottomNavigationView=findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected( MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+                        return true;
+                    case R.id.playlist:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,playlistFragment).commit();
+                        return true;
+                    case R.id.setting:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,settingFragment).commit();
+                        return true;
+
+                }
+                return false;
+
+            }
+
+
+    });
+    };
 }
+
+
+
+
