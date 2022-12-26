f53329bb1cb9aced8393affa0ff2109d0f9cd52c
Ishika Jain <ishika.jain2020@vitbhopal.ac.in>
2022-12-26 13:48:54 +0530

    Vibe

---
+++ app/src/main/java/com/example/vibe/SplashScreen.java
@@ -0,0 +1,43 @@
+package com.example.vibe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.WindowManager;
+import android.widget.VideoView;
+
+public class SplashScreen extends MainActivity {
+VideoView videoView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        videoView= findViewById(R.id.videoView);
+
+        String uriPath = "android.resource://com.example.vibe/" + R.raw.vibe_1;
+        Uri uri = Uri.parse(uriPath);
+        videoView.setVideoURI(uri);
+        videoView.requestFocus();
+        videoView.start();
+//  imgAnim.setVideoPath("android.resource://com.myapplication/" + R.drawable.vidio);
+        int SPLASH_DISPLAY_LENGTH = 5000;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, 5000);
+    }
+
+
+
+
+}
